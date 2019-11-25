f6a11d0b403088600d19cc34b87c6042a82a7caf
denis-kholmogorov <kholmogorov.denis@gmail.com>
2019-11-25 16:24:13 +0300

    version 1.0

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -1,9 +1,7 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CSVReader {
 
@@ -12,46 +10,59 @@ public class CSVReader {
 
     private static HashMap<String, Double> map = new HashMap<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
-        double localExpenses = 0;
+        double localExpenses;
 
-        try {
+        try
+        {
             List<String> lines = Files.readAllLines(Paths.get("resourse/movementList.csv"));
             lines.remove(0);
-            for (String line : lines) {
+
+            for (String line : lines)
+            {
                 String[] fragments = line.split(",", 8);
-                try {
+
+                try
+                {
                     localExpenses = Double.parseDouble(fragments[7]);
                     expenses = expenses + localExpenses;
-                }catch (NumberFormatException e){
-
+                }
+                catch (NumberFormatException e)
+                {
                     localExpenses = Double.parseDouble(fragments[7].replace("\"", "")
                                         .replace(",","."));
                     expenses = expenses + localExpenses;
-
                 }
+
                 income = income + Double.parseDouble(fragments[6]);
 
                 String[] a = fragments[5].split("    ",3);
 
-                if(a[1].matches(".+\\\\.+")) {
+                if(a[1].matches(".+\\\\.+"))
+                {
                     String[] b = a[1].split("\\\\");
                     String key = b[b.length-1].strip();
-                    if(!map.containsKey(key)){
+                    if(!map.containsKey(key))
+                    {
                         map.put(key, localExpenses);
                     }
-                    else{
+                    else
+                    {
                         map.put(key, map.get(key) + localExpenses);
                     }
                 }
             }
-        } catch (IOException e) {
+        } catch (IOException e)
+        {
             e.printStackTrace();
         }
-        for(String key: map.keySet()){
+
+        for(String key: map.keySet())
+        {
             System.out.println("Расходавано на " + key + " --- " + map.get(key) + " руб");
         }
-        System.out.println(income + " " + expenses);
+        System.out.println("Общий доход - " + income + "\nОбщий расход - " + expenses);
     }
 }
